4c73aed155f7cc77dd60ffaf2fb771ee1a0dcff0
Constantin Pantesi <constantin.pantesi@yahoo.com>
2023-09-20 17:42:15 +0200

    Add project files.

---
+++ OneAssignmentFiveTethods/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string? MyFirstMethod() 
+{
+    Console.WriteLine("Enter a name, please!");
+    string userInput = Console.ReadLine();
+    return userInput;
+}
+
+
+string? MySecondMethod()
+{
+    Console.WriteLine("Enter a word, please!");
+    string userInput = Console.ReadLine();
+    return ("#" + userInput);
+}
+
+
+string? MyThirdMethod(string message)
+{
+    Console.WriteLine(message);
+    string userInput = Console.ReadLine();
+    return userInput;
+}
+
+
+string? MyFourthMethod(string word)
+{
+    Console.WriteLine($"Please enter a {word}");
+    string userInput = Console.ReadLine();
+    return userInput;
+}
+
+string? MyFifthMethod(string firstName, string lastName)
+{
+    Console.WriteLine("Please enter your first name");
+    string userInputFirstName = Console.ReadLine();
+    Console.WriteLine("Please enter your last name");
+    string userInputLastName = Console.ReadLine();
+    string userInputFullName = (userInputLastName + ", " + userInputFirstName);
+    return userInputFullName;
+}
+
+
+Console.WriteLine("userInput: " + MyFirstMethod());
+Console.WriteLine(MySecondMethod());
+Console.WriteLine(MyThirdMethod("Please enter a number"));
+Console.WriteLine(MyFourthMethod("number"));
+Console.WriteLine(MyFifthMethod("Please enter your first name", "Please enter your last name"));
+
+
